ebbfcf6b38918763dc163f9f662c83045576e2d1
khemraj <khemraj@nestaway.com>
2015-10-31 11:47:53 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KhemrajApp::Application.config.secret_token = 'd674d301d48a598486d797ea36f828ebff5052d3d16c7848cfcc7cc74c05c05fc4e1e5c0f3582ab2567ff0bc8b1415e481b8518d863f613d41fb47b4735ee7a7'
